253a4ea16516dbc0c8e5a0f07ae639c5a532f6e4
blackenedgem <Chrisroberts97@btinternet.com>
2018-07-24 01:20:32 +0300

    API initial request

--- retriever.py
+++ retriever.py
@@ -3,16 +3,20 @@ import requests
 
 from switches import Switches
 
+# Constants
+API_HISTORY_DM = "https://slack.com/api/im.history"
+
 # Get args
 parser = argparse.ArgumentParser()
 parser.add_argument('token')
 parser.add_argument('channel')
 args = parser.parse_args()
-
-
 Switches.set_switches(args, parser)
 
 # Get message history
-print(Switches.dateMode)
-# Get files
-print(args)
+params = {
+    'token': args.token,
+    'channel': args.channel
+}
+response = requests.get(API_HISTORY_DM, params)
+print(response.content)
