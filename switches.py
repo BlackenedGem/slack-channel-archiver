3d97d2404768f45c2f2decb5bbf10894a71d2586
blackenedgem <Chrisroberts97@btinternet.com>
2018-07-24 13:12:43 +0300

    Interpret date start/date end

--- switches.py
+++ switches.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import argparse
+import datetime
 
 class Switches:
     # region Switch definitions
@@ -7,6 +8,8 @@ class Switches:
         ISO8601 = '%Y-%m-%d'
         UK = '%d/%m/%Y'
     dateMode = dateModes.ISO8601
+    dateStart = None
+    dateEnd = None
     # endregion
 
     # Set using arguments
@@ -14,8 +17,21 @@ class Switches:
     def set_switches(cls, args: argparse.Namespace, parser: argparse.ArgumentParser):
         if 'date_format' in args:
             cls.dateMode = cls.convert_enum(cls.dateModes, args.date_format, "date format", parser)
+        if 'date_start' in args:
+            cls.dateStart = cls.convert_date(args.date_start, parser)
+        if 'date_end' in args:
+            cls.dateEnd = cls.convert_date(args.date_end, parser)
 
-    # Method to handle parsing switches properly
+    # Handle date parsing
+    @classmethod
+    def convert_date(cls, date_str: str, arg_parser: argparse.ArgumentParser):
+        try:
+            return datetime.datetime.strptime(date_str, cls.dateMode.value)
+        except ValueError as e:
+            arg_parser.error(e)
+
+
+    # Handle parsing switches properly
     @classmethod
     def convert_enum(cls, enum, string: str, switch_str: str, arg_parser: argparse.ArgumentParser):
         try:
