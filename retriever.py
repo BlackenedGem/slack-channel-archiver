18df8c9e5d14f96c9c2f19389c0b0116ccb710dd
blackenedgem <Chrisroberts97@btinternet.com>
2018-07-24 17:36:11 +0300

    Option to output as json

--- retriever.py
+++ retriever.py
@@ -1,14 +1,20 @@
 import argparse
+import os.path
+import json
+import sys
 
 from switches import Switches
 from api import Api
 
-# Setup argparse
+# region Argparse setup
+# Required args
 parser = argparse.ArgumentParser()
 parser.add_argument('token',
                     help="Slack authorisation token")
 parser.add_argument('dm',
                     help="ID of the direct message chat")
+
+# Date args
 parser.add_argument('-df', '--date-format',
                     help="Date format to use. Supported options: " + Switches.list_enum(Switches.DateModes))
 parser.add_argument('-ds', '--date-start',
@@ -16,13 +22,31 @@ parser.add_argument('-ds', '--date-start',
 parser.add_argument('-de', '--date-end',
                     help="Latest messages to archive (inclusive)")
 
+# Export args
+parser.add_argument('-o', '--output', nargs='?', const='', default='',
+                    help="Output directory to use")
+parser.add_argument('-j', '--json', action='store_const', const='dm.json',
+                    help="Output the message history in raw json form")
+
 # Process basic args
 args = parser.parse_args()
 Switches.set_switches(args, parser)
 Api.token = args.token
 
-# Retrieve messages
+# Retrieve messages and write to json if requested
 messages = Api.get_dm_history(args.dm, Switches.date_start, Switches.date_end)
+messages.reverse()
+
+if args.json is not None:
+    out_file = os.path.join(args.output + args.json)
+    print("Writing raw JSON to: " + out_file)
+
+    # noinspection PyBroadException
+    try:
+        open(out_file, 'w').write(json.dumps(messages, indent=4))
+    except Exception as e:
+        print(e)
+
 
 # Mapping of user ids to display names
 user_map = {}
@@ -30,3 +54,4 @@ user_ids = set(x['user'] for x in messages)
 for user_id in user_ids:
     user_map[user_id] = Api.get_username(user_id)
 
+print(json.dumps(messages, indent=4))
